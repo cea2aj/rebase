329755b8d1a019ecec466dbc98dff2e78bb234e4
Connor Anderson <canderson@yext.com>
2022-04-13 18:02:13 -0400

    add safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,6 +83,8 @@ USER_TOKEN=${USER_LOGIN//-/_}_TOKEN
 UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
 COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
 
+git config --global --add safe.directory /github/workspace
+
 git remote set-url origin https://x-access-token:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
